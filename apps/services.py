058ee9557c2ee026235616eb2d1baceaa4b158dd
nakano shota <nakano.syota.0405@gmail.com>
2018-07-01 13:35:47 +0900

    Add lt register form

--- apps/services.py
+++ apps/services.py
@@ -3,5 +3,11 @@ def get_home_message():
     ホーム画面で表示するメッセージの取得
     :return:
     """
-    # todo DBから取得する
-    return "次回のLT会は7/27(金)です。参加者募集中です"
+    # todo 期間から該当するLTを取得し、参加表明済みか否かによってメッセージを変更する
+    is_applied = False
+    message = "次回のLT会は7/27(金)です"
+    return {"message": message, "is_applied": is_applied}
+
+
+def register_lt(title, time):
+    print(f"{title}, {time}")
