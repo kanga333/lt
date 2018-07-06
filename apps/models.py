40a7b9ddf0dc3ec66e53407a054d84267d4b7e08
nakano shota <nakano.syota.0405@gmail.com>
2018-07-07 00:01:33 +0900

    Add models

--- apps/models.py
+++ apps/models.py
@@ -11,5 +11,20 @@ class User(db.Model):
         return f"<User> id={self.id}, name={self.name}, email={self.email}"
 
 
+class Lt(db.Model):
+    __tablename__ = 'lt'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Text)
+
+
+class Talk(db.Model):
+    __tablename__ = 'talk'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    lt_id = db.Column(db.Integer)
+    title = db.Column(db.Text)
+    presentation_time = db.Column(db.Integer)
+
+
 def init():
     db.create_all()
