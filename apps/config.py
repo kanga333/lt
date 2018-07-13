3f345eff263af0308734fa9811f69263cc2a2168
kanga333 <e411z7t40w@gmail.com>
2018-07-13 09:44:38 +0900

    Introduce env

--- apps/config.py
+++ apps/config.py
@@ -1,8 +1,10 @@
+import os
+
 class DevConfig:
-    __user = "root"
-    __password = ""
-    __host = "localhost"
-    __db_name = "lt"
+    __user = os.getenv('DB_USER', 'root')
+    __password = os.getenv('DB_PASSWORD', '')
+    __host = os.getenv('DB_HOST', 'localhost')
+    __db_name = os.getenv('DB_NAME', 'lt')
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{__user}:{__password}@{__host}/{__db_name}"
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
